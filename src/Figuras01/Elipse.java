3d38793cd861886558dc62ad01622be15e080ba9
Leonardo <leonardo38361@gmail.com>
2024-04-28 23:09:58 -0300

    clase Elipse

---
+++ src/Figuras01/Elipse.java
@@ -0,0 +1,31 @@
+package Figuras01;
+
+import java.util.List;
+
+/**
+ * @author mrRobot
+ * @version 1.0
+ * @created 28-Apr-2024 5:26:50 PM
+ */
+public class Elipse implements Figura {
+
+
+	public Elipse(){
+
+	}
+
+	public double calcularArea() {
+		return 0;
+	}
+
+	@Override
+	public double calcularPerimetro() {
+		return 0;
+	}
+
+	@Override
+	public void agregarLados(List<Lado> lado) {
+
+	}
+
+}
