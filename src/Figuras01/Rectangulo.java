1914aacfc5f2bc9c3f7b0797645c2aa3d23674f3
Leonardo <leonardo38361@gmail.com>
2024-04-28 23:12:59 -0300

    Rectangulo

---
+++ src/Figuras01/Rectangulo.java
@@ -0,0 +1,14 @@
+package Figuras01;
+
+/**
+ * @author mrRobot
+ * @version 1.0
+ * @created 28-Apr-2024 5:26:50 PM
+ */
+public class Rectangulo extends Cuadrilatero {
+
+	public Rectangulo(){
+		
+	}
+
+}
