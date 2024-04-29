3c1ec2320136f09a78e4ff22925760b2e829f929
Leonardo <leonardo38361@gmail.com>
2024-04-28 23:09:13 -0300

    clase cuadrado

---
+++ src/Figuras01/Cuadrado.java
@@ -0,0 +1,14 @@
+package Figuras01;
+
+/**
+ * @author mrRobot
+ * @version 1.0
+ * @created 28-Apr-2024 5:26:51 PM
+ */
+public class Cuadrado extends Rectangulo {
+
+	public Cuadrado(){
+		
+	}
+
+}
