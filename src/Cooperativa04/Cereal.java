f3f79007bea3da053f3ea21d145847c63ad2a1db
Leonardo <leonardo38361@gmail.com>
2024-05-06 02:45:44 -0300

    clase Cereal

---
+++ src/Cooperativa04/Cereal.java
@@ -0,0 +1,56 @@
+package Cooperativa04;
+
+import java.util.List;
+
+/**
+ * @author mrRobot
+ * @version 1.0
+ * @created 06-May-2024 1:04:47 AM
+ */
+public abstract class Cereal {
+
+	protected String nombre;
+	protected List<Mineral> requerimientoDeMinerales;
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public Cereal(String nombre, List<Mineral> requerimientoDeMinerales) {
+		this.nombre = nombre;
+		this.requerimientoDeMinerales = requerimientoDeMinerales;
+	}
+
+	public boolean puedeSembrar(Lote lote) {
+		for(Mineral mineral : requerimientoDeMinerales) {
+			if(!lote.getMineral().contains(mineral)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * 
+	 * @param newVal
+	 */
+	public void setNombre(String newVal) {
+		nombre = newVal;
+	}
+
+	public List<Mineral> getRequerimientoDeMinerales() {
+		return requerimientoDeMinerales;
+	}
+
+	public void setRequerimientoDeMinerales(List<Mineral> requerimientoDeMinerales) {
+		this.requerimientoDeMinerales = requerimientoDeMinerales;
+	}
+	
+	
+
+}
